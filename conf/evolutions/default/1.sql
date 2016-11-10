e632eb274db1e0aae8925119c4ca39ff39bbb350
annadowling <anna.dowling@errigal.com>
2016-11-10 20:44:52 +0000

    local app using remote database

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,18 +4,16 @@
 # --- !Ups
 
 create table my_user (
-  id                            bigint not null,
+  id                            bigserial not null,
   firstname                     varchar(255),
   lastname                      varchar(255),
   email                         varchar(255),
   password                      varchar(255),
   constraint pk_my_user primary key (id)
 );
-create sequence my_user_seq;
 
 
 # --- !Downs
 
-drop table if exists my_user;
-drop sequence if exists my_user_seq;
+drop table if exists my_user cascade;
 
